504d986ca3b8a2fab92bdb70cdc5962b14d2cf8e
voidplus <mail@voidplus.de>
2015-02-03 19:24:46 +0100

    initialized continuous integration (travis)

--- src/test/java/de/voidplus/soundcloud/SoundCloudTest.java
+++ src/test/java/de/voidplus/soundcloud/SoundCloudTest.java
@@ -2,14 +2,24 @@ package de.voidplus.soundcloud;
 
 import static org.junit.Assert.*;
 
+import java.util.Map;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class SoundCloudTest {
 
+	private static Map<String, String> ENV;
+	
+    @BeforeClass
+    public static void setUp() {
+    	ENV = System.getenv();
+    }
+	
 	@Test
 	public void testLogin() {
-		SoundCloud sc = new SoundCloud(Settings.APP_ID, Settings.APP_SECRET);
-		boolean login = sc.login(Settings.LOGIN_MAIL, Settings.LOGIN_PASS);
+		SoundCloud sc = new SoundCloud(ENV.get("APP_ID"), ENV.get("APP_SECRET"));
+		boolean login = sc.login(ENV.get("LOGIN_MAIL"), ENV.get("LOGIN_PASS"));
 		assertTrue("login", login);
 	}
 
