c672d3530ae92a5a7ef6ff39ba1bb835612955dd
Darius Morawiec <mail@nok.onl>
2015-02-03 19:42:23 +0100

    encrypted login pw

--- src/test/java/de/voidplus/soundcloud/SoundCloudTest.java
+++ src/test/java/de/voidplus/soundcloud/SoundCloudTest.java
@@ -2,24 +2,14 @@ package de.voidplus.soundcloud;
 
 import static org.junit.Assert.*;
 
-import java.util.Map;
-
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class SoundCloudTest {
-
-	private static Map<String, String> ENV;
-	
-    @BeforeClass
-    public static void setUp() {
-    	ENV = System.getenv();
-    }
 	
 	@Test
 	public void testLogin() {
-		SoundCloud sc = new SoundCloud(ENV.get("APP_ID"), ENV.get("APP_SECRET"));
-		boolean login = sc.login(ENV.get("LOGIN_MAIL"), ENV.get("LOGIN_PASS"));
+		SoundCloud sc = new SoundCloud(System.getenv("APP_ID"), System.getenv("APP_SECRET"));
+		boolean login = sc.login(System.getenv("LOGIN_MAIL"), System.getenv("LOGIN_PASS"));
 		assertTrue("login", login);
 	}
 
